24ebdfcc1e97eef5e45fe6f9d587f06e4396d387
Mac Topp <macm.topp@gmail.com>
2019-10-06 14:42:27 -0600

    maybe this will work

---
+++ public/build/precache-manifest.7aa20d87dbfd426ef5e50b6f367271ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3da826e0e5d2054e93caed17b157108",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2960b245f943d5c49b11",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "7d54db8c70461a1e6cab",
+    "url": "/static/js/2.099231b9.chunk.js"
+  },
+  {
+    "revision": "2960b245f943d5c49b11",
+    "url": "/static/js/main.cbe07815.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "6b7cbac7b67058e2ee065f596909093c",
+    "url": "/static/media/SpaceX.6b7cbac7.png"
+  }
+]);
---
+++ public/precache-manifest.81124e1ff799482fc06faa773d85fb48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b439131181d4530686d3232f47e9960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "767eba91bf3175b56c4c",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "6eccf236974a4f92d96c",
+    "url": "/static/js/2.a70f16e2.chunk.js"
+  },
+  {
+    "revision": "767eba91bf3175b56c4c",
+    "url": "/static/js/main.8fd8f096.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "6b7cbac7b67058e2ee065f596909093c",
+    "url": "/static/media/SpaceX.6b7cbac7.png"
+  }
+]);
